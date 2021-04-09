d510acb8c2163ad6a9938f008358472a1f307eed
princessfruitt <borisova.alex9@gmail.com>
2021-04-09 14:54:20 +0300

    get content from github

--- go.mod
+++ go.mod
@@ -3,8 +3,9 @@ module github.com/princessfruittt/ansitotosca
 go 1.15
 
 require (
+	github.com/frankban/quicktest v1.11.2
 	github.com/gohugoio/hugo v0.80.0
-	github.com/google/go-github/v33 v33.0.0
+	github.com/google/go-github/v34 v34.0.0
 	github.com/spf13/cobra v1.1.1
 	github.com/spf13/jwalterweatherman v1.1.0
 	golang.org/x/sys v0.0.0-20210124154548-22da62e12c0c // indirect
--- pkg/cmd/generate.go
+++ pkg/cmd/generate.go
@@ -2,11 +2,16 @@ package cmd
 
 import (
 	"context"
+	"crypto/sha1"
+	"encoding/hex"
 	"fmt"
-	"github.com/google/go-github/v33/github"
+	"github.com/google/go-github/v34/github"
 	"github.com/spf13/cobra"
+	"io/ioutil"
 	"log"
 	"net/url"
+	"path/filepath"
+	"strconv"
 	"strings"
 )
 
@@ -36,10 +41,85 @@ func (c *generateCmd) generateTypes(cmd *cobra.Command, args []string) {
 	}
 	fmt.Println(u.Path)
 	info := strings.Split(u.Path, "/")
-	opt, _, err := client.Repositories.Get(ctx, info[1], info[2])
+	getContents(ctx, client, info[1], info[2], "", "")
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	fmt.Println(opt.GetSource().Source)
+}
+
+func getContents(ctx context.Context, client *github.Client, owner string, repo string, path string, parentDirName string) {
+	fileContent, directoryContent, response, err := client.Repositories.GetContents(ctx, owner, repo, path, nil)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("%#v\n", fileContent)
+	fmt.Printf("%#v\n", directoryContent)
+	fmt.Printf("%#v\n", response)
+
+	for _, c := range directoryContent {
+		fmt.Println(*c.Type, *c.Size, *c.SHA)
+
+		switch *c.Type {
+		case "file":
+			switch parentDirName {
+			case "templates":
+				fmt.Println(parentDirName)
+				downloadContents(ctx, owner, repo, client, c)
+			case "defaults":
+				fmt.Println(parentDirName)
+				downloadContents(ctx, owner, repo, client, c)
+			case "handlers":
+				fmt.Println(parentDirName)
+				downloadContents(ctx, owner, repo, client, c)
+			case "meta":
+				fmt.Println(parentDirName)
+				downloadContents(ctx, owner, repo, client, c)
+			case "tasks":
+				fmt.Println(parentDirName)
+				downloadContents(ctx, owner, repo, client, c)
+			case "vars":
+				fmt.Println(parentDirName)
+				downloadContents(ctx, owner, repo, client, c)
+			}
+
+		case "dir":
+			getContents(ctx, client, owner, repo, filepath.Join(path, *c.Path), *c.Name)
+		}
+	}
+}
+
+func downloadContents(ctx context.Context, owner string, repo string, client *github.Client, content *github.RepositoryContent) {
+	rc, _, _, err_download := client.Repositories.DownloadContentsWithMeta(ctx, owner, repo, *content.Path, nil)
+	if err_download != nil {
+		fmt.Println(err_download)
+		return
+	}
+	defer rc.Close()
+	var b, err_read = ioutil.ReadAll(rc)
+	if err_read != nil {
+		fmt.Println(err_read)
+		return
+	}
+	sha := calculateGitSHA1(b)
+	if *content.SHA == hex.EncodeToString(sha) {
+		fmt.Println("no need to update this file, the SHA is the same")
+		fmt.Print(string(b))
+	} else {
+		fmt.Println("SHA err")
+	}
+
+}
+
+func calculateGitSHA1(contents []byte) []byte {
+	contentLen := len(contents)
+	blobSlice := []byte("blob " + strconv.Itoa(contentLen))
+	blobSlice = append(blobSlice, '\x00')
+	blobSlice = append(blobSlice, contents...)
+	h := sha1.New()
+	h.Write(blobSlice)
+	bs := h.Sum(nil)
+	return bs
 }
